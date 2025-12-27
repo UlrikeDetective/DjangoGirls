e0b8a1064d3a969b487e0ccc5e2964e2acaeb407
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2025-12-27 17:37:44 +0100

    update comment approved

--- blog/views.py
+++ blog/views.py
@@ -50,6 +50,18 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
